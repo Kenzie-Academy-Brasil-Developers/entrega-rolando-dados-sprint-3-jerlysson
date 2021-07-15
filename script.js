21ebabf3aed50e93504d9e93b37426c79b1240ba
Jerlysson Miranda <jerlyssonm@gmail.com>
2021-07-15 20:46:59 -0300

    melhoria no codigo

--- script.js
+++ script.js
@@ -7,31 +7,6 @@ const rodarOs2 = document.getElementById('rodar');
 const boxResult = document.getElementById('boxResult');
 const btnMil = document.getElementById('btnMil');
 
-// =======;====================
-let div2 ;
-let div3 ;
-let div4 ;
-let div5 ;
-let div6 ;
-let div7 ;
-let div8 ;
-let div9 ;
-let div10 ;
-let div11 ;
-let div12 ;
-
-let dois = 0;
-let tres = 0;
-let quatro = 0
-let cinco = 0;
-let seis = 0;
-let sete = 0;
-let oito = 0;
-let nove = 0;
-let dez = 0;
-let onze = 0;
-let doze= 0;
-
 // ===========================================================
 
 let result1 = 1;
@@ -58,78 +33,42 @@ const RodarOs2Dados = () => {
    girarDado2();
 }
 
-const addResult = (span, paran1) => {
-    let li = document.createElement('li');
-    span = document.createElement("span");
-    span.innerText = paran1;
-    span.style.color = 'white';
-    li.style.width = 20+paran1 +'px';
-    li.style.border = "1px solid black";
-    li.style.background = 'green';
-    li.style.fontSize= '15px';
-    li.style.textAlign= 'left';
-    li.appendChild(span);
-    boxResult.appendChild(li);
+const addResult = (paran1) => {
+    let countStr = paran1.toString();
+    countStr = countStr.replace(',',':');
+    let div = document.createElement('div');
+    div.innerText = countStr;
+    div.style.color = 'white';
+    div.style.width = 20+paran1[1]+'px';
+    div.style.border = "1px solid black";
+    div.style.background = 'green';
+    div.style.fontSize= '15px';
+    div.style.textAlign= 'left';
+    boxResult.appendChild(div);
 }
 
 const girarMil = () =>{
     boxResult.innerText="";
 
-
-dois = 0;
-tres = 0;
-quatro = 0;
-cinco = 0;
-seis = 0;
-sete = 0;
-oito = 0;
-nove = 0;
-dez = 0;
-onze = 0;
-doze= 0;
-
-    let count = [];    
-    for(let i = 0; i < 1000; i++){
+    let count = [[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0]];
+    let soma = 0;
+    for(let y = 0;y< 1000;y++){
         RodarOs2Dados()
-        count.push(result1+result2);
-    }
-    for(let i = 0; i < count.length; i++){
-        if(count[i]===2){
-            dois++;
-        }else if(count[i]===3){
-            tres++;
-        }else if(count[i]===4){
-            quatro++;
-        }else if(count[i]===5){
-            cinco++;
-        }else if(count[i]===6){
-            seis++;
-        }else if(count[i]===7){
-            sete++;
-        }else if(count[i]===8){
-            oito++;
-        }else if(count[i]===9){
-            nove++;
-        }else if(count[i]===10){
-            dez++;
-        }else if(count[i]===11){
-            onze++;
-        }else {
-            doze++
+        soma = result1+result2;
+   
+        for(let i = 0;i< count.length; i++){
+            if(soma === count[i][0]){
+                count[i][1]++
+            }
+            console.log(count)
         }
     }
-    addResult(div2,dois);
-    addResult(div3,tres);
-    addResult(div4,quatro);
-    addResult(div5,cinco);
-    addResult(div6,seis);
-    addResult(div7,sete);
-    addResult(div8,oito);
-    addResult(div9,nove);
-    addResult(div10,dez);
-    addResult(div11,onze);
-    addResult(div12,doze);
+    for(let i = 0; i < count.length;i++){
+        addResult(count[i]);
+    }
+       
 }
+
 // ===========================================================
 
 btnMil.addEventListener('click', girarMil);
